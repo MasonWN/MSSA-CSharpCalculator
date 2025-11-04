d961e9549c343356c2caf895ad6022cca6b4ad38
Mason Ngo <masonwngo@gmail.com>
2025-11-04 09:45:40 -0800

    fixed merge conflict

--- Program.cs
+++ Program.cs
@@ -8,25 +8,11 @@ public class Program
 
         Console.WriteLine("this is a new branch named test");
 
-<<<<<<< HEAD
-        Console.Write("enter your name:");
-
-        string name = Console.ReadLine();
-
-        if (name == null)
-        {
-            Console.WriteLine("No valid input revieved");
-            return;
-        }
-
-        Console.WriteLine($"Hello, {name}");
-=======
         Console.Write("Enter your favorite number:");
 
         int num = int.Parse(Console.ReadLine());
 
         Console.WriteLine($"your favorite number is {num}");
 
->>>>>>> main
     }
 }
