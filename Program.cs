9e4407233f52cb19c2bac44fe6666bfd590f8bc7
Mason Ngo <masonwngo@gmail.com>
2025-11-04 07:59:04 -0800

    updated WriteLine

--- Program.cs
+++ Program.cs
@@ -5,5 +5,7 @@ public class Program
     public static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+
+        Console.WriteLine("this is a new branch named test");
     }
 }
